90508b1c7bdf684bd4745a200d92fe51bbcfbfae
Filip Królikowski <fkrolikowski@gromar.eu>
2025-05-06 16:54:55 +0200

    add toolbar

--- app/components/Pages/Flexbox/FlexboxPage.tsx
+++ app/components/Pages/Flexbox/FlexboxPage.tsx
@@ -6,14 +6,18 @@ import LayoutGroupWrapper from "../../UI/LayoutGroup";
 import Playground from "../../Playground/Playground";
 import { useFlexbox } from "../../../hooks/useFlexbox";
 import FlexboxItem from "./FlexboxItem/FlexboxItem";
+import FlexboxToolbar from "./FlexboxToolbar/FlexboxToolbar";
 
 export default function FlexboxPage() {
   const { items, container, clearSelected } = useFlexbox();
+
+  const playgroundTools = [{ component: <FlexboxToolbar />, id: "toolbar" }];
+
   return (
     <MainContent>
       <LayoutGroupWrapper>
         <FlexboxSidebar />
-        <Playground container={container}>
+        <Playground container={container} playgroundTools={playgroundTools}>
           {items.map((item) => (
             <FlexboxItem key={item.id} item={item} />
           ))}
--- app/components/Playground/Playground.tsx
+++ app/components/Playground/Playground.tsx
@@ -3,17 +3,22 @@
 import styles from "./Playground.module.scss";
 import { AnimatePresence, motion } from "framer-motion";
 import Resizable from "../UI/Resizable/Resizable";
+import { Fragment } from "react";
 import { Container } from "../../types";
 
 interface Props {
   children: React.ReactNode;
   container: Container;
+  playgroundTools?: { component: React.ReactNode; id: string }[];
 }
 
-function Playground({ children, container }: Props) {
+function Playground({ children, playgroundTools, container }: Props) {
   return (
     <motion.div layout className={styles.playground_wrapper}>
       <Resizable className={styles.playground}>
+        {playgroundTools?.map((tool: { component: React.ReactNode; id: string }) => (
+          <Fragment key={tool.id}>{tool.component}</Fragment>
+        ))}
         <motion.div layout className={styles.playground_view} id="playground_view" style={container}>
           <AnimatePresence mode="popLayout">{children}</AnimatePresence>
         </motion.div>
